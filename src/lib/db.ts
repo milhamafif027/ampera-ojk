1e9ac66186bcfe8e087009c440c78082fc8c8717
afif <milhamafif027@gmail.com>
2026-08-27 10:54:19 +0700

    fix: migrate database to prisma and supabase successfully

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,7 @@
-import mysql from "mysql2/promise";
+import { PrismaClient } from "@prisma/client";
 
-export const db = mysql.createPool({
-  host: process.env.DATABASE_HOST || "localhost",
-  port: Number(process.env.DATABASE_PORT) || 3306,
-  user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "",
-  database: process.env.DATABASE_NAME || "ampera_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const db = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
